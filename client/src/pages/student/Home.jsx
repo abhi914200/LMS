8312e992154ac841d0828d7f781eb4dab7b90e9d
Abhimanyu Raj <abhimanyu.12g@gmail.com>
2025-07-03 11:08:57 +0530

    navbar-created and SignIn feature using clerk and homepage-start

---
+++ client/src/pages/student/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Hero  from './../../components/student/Hero'
+import Companies from '../../components/student/Companies'
+const Home = () => {
+  return (
+    <div>
+      <Hero />
+      <Companies />
+    </div>
+  )
+}
+
+export default Home
