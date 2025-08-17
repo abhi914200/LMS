4193a314638a8a3a8062c10b020f61b522d6a129
Abhimanyu Raj <abhimanyu.12g@gmail.com>
2025-08-17 14:10:05 +0530

    Refactor components and add CoursesSection with CourseCard; update AppContext for course fetching; enhance Navbar structure.

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,12 +1,27 @@
-import {createContext} from 'react'
+import {createContext, useEffect, useState} from 'react'
+import { dummyCourses } from '../assets/assets';
+import { useNavigate } from 'react-router-dom';
+
 
 export const AppContext=createContext();
 export const AppContextProvider=(props)=>{
+  const currency=import.meta.env.VITE_CURRENCY;
+  const[allcourses,setallcourses]=useState([]);
+
+   const navigate=useNavigate();
+  //fetch all courses
+  const fetchAllcourses=async()=>{
+    setallcourses(dummyCourses);
+  }
+  useEffect(()=>{
+    fetchAllcourses()
+  },[]);
+
     const value={
-        
+        currency,allcourses,navigate
     }
     return(
-        <AppContext.Provider>
+        <AppContext.Provider value={value}>
           {props.children}
         </AppContext.Provider>
     )
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -10,8 +10,12 @@ export default {
         'course-details-heading-small':['26px','36px'],
         'course-details-heading-large':['36px','44px'],
         'home-heading-small':['28px','36px'],
-        'home-heading-large':['36px','44px']
-      }
+        'home-heading-large':['36px','44px'],
+        default:['15px','21px']
+      },
+     gridTemplateColumns:{
+        'auto' : 'repeat(auto-fit, minmax(200px,1fr))',
+      },
     },
   },
   plugins: [],
